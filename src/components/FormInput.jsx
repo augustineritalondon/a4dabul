af0d4724ce7ccb7a2bf9c81bb50f334cd3f8d501
augutineritalondon <augutineritalondon@gmail.com>
2024-02-02 15:48:07 +0100

    added redirect file

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FormInput = ({ type, placeholder, name }) => {
     return (
         <>
-            <input type={type} placeholder={placeholder} name={name} className='appearance-none w-full rounded-full border border-grey text-e-14 py-3 focus:border-grey placeholder:text-grey my-2 bg-transparent px-5 focus:ring-0' />
+            <input type={type} placeholder={placeholder} name={name} className='appearance-none w- w-full rounded-full border border-grey text-e-14 py-3 focus:border-grey placeholder:text-grey my-2 bg-transparent px-5 focus:ring-0' />
         </>
     )
 }
